3dd7b097524d04bdbccf831ec0034d570db52b7d
Pennan <1209036311@qq.com>
2017-01-03 13:59:38 +0800

    添加了无触摸时变透明的功能，以及移动贴边的功能

--- app/src/main/java/com/np/movableview/MovableView.java
+++ app/src/main/java/com/np/movableview/MovableView.java
@@ -1,23 +1,40 @@
 package com.np.movableview;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.os.CountDownTimer;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewGroup;
 
 /**
  * 功能：
- * ①、可随手指的移动而移动
- * ②、点击的时候只响应点击事件，滑动的时候只响应滑动事件
- * ③、进行边界控制，只允许在屏幕内移动.
+ * 1、可随手指的移动而移动
+ * 2、无事件时，过段时间自动透明
+ * 3、点击的时候只响应点击事件，滑动的时候只响应滑动事件
+ * 4、进行边界控制，只允许在屏幕内移动.
+ * 5、松开后自动回到屏幕左侧或右侧
  */
 public class MovableView extends View {
     private static final String TAG = "MovableView";
     private int mTouchSlop; // 系统认为的最小滑动距离
     private int screenWidth;
     private int screenHeight;
+
+    private MyCountDownTimer countDownTimer;
+    private long millisInFuture = 2500;
+    private long countDownInterval = 500;
+
+    private float toAlpha = 0.2f;
+
+    // 可以移动的范围
+    private int minLeftMargin;
+    private int maxLeftMargin;
+    private int minTopMargin;
+    private int maxTopMargin;
 
     public MovableView(Context context) {
         this(context, null);
@@ -34,6 +51,9 @@ public class MovableView extends View {
         screenWidth = metrics.widthPixels;
         // metrics.heightPixels(屏幕高度) = 屏幕空白处高度 + 顶部状态栏(不包括底部导航栏)
         screenHeight = metrics.heightPixels - getStatusHeight();
+
+        countDownTimer = new MyCountDownTimer(millisInFuture, countDownInterval);
+        countDownTimer.start();
     }
 
     int lastX;
@@ -47,6 +67,9 @@ public class MovableView extends View {
         int currY = (int) event.getRawY();
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                setAlpha(1f);
+                countDownTimer.cancel();
+
                 rawX = lastX = currX;
                 rawY = lastY = currY;
                 break;
@@ -58,11 +81,24 @@ public class MovableView extends View {
                 lastY = currY;
                 break;
             case MotionEvent.ACTION_UP:
+                countDownTimer.start();
                 // 处理点击事件和移动事件
                 if (Math.abs(lastX - rawX) < mTouchSlop &&
                         Math.abs(lastY - rawY) < mTouchSlop) {
                     performClick();
                 }
+
+                // 停止移动时，使 View 贴边.
+                ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) getLayoutParams();
+                int fromLeftMargin = lp.leftMargin;
+                if ((getLeft() + getMeasuredWidth() / 2) <= screenWidth / 2) {
+                    lp.leftMargin = minLeftMargin;
+                } else {
+                    lp.leftMargin = maxLeftMargin;
+                }
+                ObjectAnimator marginChange = ObjectAnimator.ofInt(new Wrapper(this), "leftMargin", fromLeftMargin, lp.leftMargin);
+                marginChange.setDuration(500);
+                marginChange.start();
                 break;
         }
 
@@ -71,14 +107,19 @@ public class MovableView extends View {
 
     private void moveView(int offsetX, int offsetY) {
         // 方法一：
-        /*ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) getLayoutParams();
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) getLayoutParams();
         int left = params.leftMargin + offsetX;
         int top = params.topMargin + offsetY;
+
+        left = left < minLeftMargin ? minLeftMargin : left;
+        left = left > maxLeftMargin ? maxLeftMargin : left;
+        top = top < minTopMargin ? minTopMargin : top;
+        top = top > maxTopMargin ? maxTopMargin : top;
 
         params.leftMargin = left;
         params.topMargin = top;
         setLayoutParams(params);
-        requestLayout();*/
+        requestLayout();
         // 方法二：
         /*offsetLeftAndRight(offsetX);
         offsetTopAndBottom(offsetY);*/
@@ -94,7 +135,7 @@ public class MovableView extends View {
         // 方法四：
         /*((View) getParent()).scrollBy(-offsetX, -offsetY);*/
         // 方法五：做了屏幕边界控制
-        int left = getLeft() + offsetX;
+        /*int left = getLeft() + offsetX;
         int top = getTop() + offsetY;
         int right = getRight() + offsetX;
         int bottom = getBottom() + offsetY;
@@ -106,7 +147,15 @@ public class MovableView extends View {
             top = getTop() - offsetY;
             bottom = getBottom() - offsetY;
         }
-        layout(left, top, right, bottom);
+        layout(left, top, right, bottom);*/
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        minLeftMargin = 0;
+        maxLeftMargin = screenWidth - getMeasuredWidth();
+        minTopMargin = 0;
+        maxTopMargin = screenHeight - getMeasuredHeight();
     }
 
     private int getStatusHeight() {
@@ -121,5 +170,48 @@ public class MovableView extends View {
             e.printStackTrace();
         }
         return statusBarHeight;
+    }
+
+    /**
+     * 使 View 能够执行属性动画的 包装类.
+     */
+    class Wrapper {
+        private View mTarget;
+
+        public Wrapper(View view) {
+            mTarget = view;
+        }
+
+        public int getLeftMargin() {
+            ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) mTarget.getLayoutParams();
+            return lp.leftMargin;
+        }
+
+        public void setLeftMargin(int leftMargin) {
+            ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) mTarget.getLayoutParams();
+            lp.leftMargin = leftMargin;
+            mTarget.requestLayout();
+        }
+    }
+
+    class MyCountDownTimer extends CountDownTimer {
+
+        /**
+         * @param millisInFuture    倒计时时间:
+         *                          调用 start() 方法开始倒计时，经过 millisInFuture 秒时间(倒计时结束),
+         *                          倒计时结束后调用 onFinish() 方法.
+         * @param countDownInterval 在倒计时(millisInFuture)这段时间里,每隔 countDownInterval 段时间执行一次 onTick() 方法.
+         */
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {}
+
+        @Override
+        public void onFinish() {
+            setAlpha(toAlpha);
+        }
     }
 }
